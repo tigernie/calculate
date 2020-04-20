d7af7abb9ce149820685f6bbaabdff09a6b7252c
nietai <841952958@qq.com>
2020-04-20 14:26:08 +0800

    version 1.0

---
+++ demo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by Nie.
+ * Date: 2020-4-20
+ * Time: 14:23
+ */
+
+require_once './vendor/autoload.php';
+use Calculate\Tools;
---
+++ src/Tools.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by Nie.
+ * Date: 2020-4-20
+ * Time: 14:14
+ */
+
+namespace Calculate;
+
+class Tools
+{
+
+}
